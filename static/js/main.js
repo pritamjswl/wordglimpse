4fd7cff638dd102da8b59dd79f3b71553a32fd3e
Pritam Jaiswal <pritamjswl@users.noreply.github.com>
2024-06-09 06:22:24 +0000

    Minor update

--- static/js/main.js
+++ static/js/main.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.setAttribute('data-bs-theme', theme);
     themeToggler.checked = (theme === 'dark');
   }
-
   themeToggler.addEventListener('change', () => {
     let newTheme = themeToggler.checked ? 'dark' : 'light';
     document.body.setAttribute('data-bs-theme', newTheme);
     localStorage.setItem('theme', newTheme);
   });
+  
 });
 
 function toggleSearch() {
