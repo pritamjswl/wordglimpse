fbc7b71130d3131e10c3eb8c427478cfa8e2e6d9
Pritam Jaiswal <156157726+pritamjswl@users.noreply.github.com>
2024-06-21 10:57:23 +0000

    minor

--- app.py
+++ app.py
@@ -5,19 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", activetab="Home")
-
-@app.route("/recent")
-def recent():
-    return render_template("recent.html", activetab="Recent")
-
-@app.route("/saved")
-def saved():
-    return render_template("saved.html", activetab="Saved")
-
-@app.route("/account")
-def account():
-    return render_template("account.html", activetab="Account")
+    return render_template("index.html")
 
 
 
