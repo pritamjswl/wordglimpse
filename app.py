e2c678ebfff61d0074a6bca78b94a2aa38adbfde
Pritam Jaiswal <156157726+pritamjswl@users.noreply.github.com>
2024-06-24 10:49:09 +0000

    Adding search layout

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search", methods=["GET"])
+def search():
+    return render_template("search.html", contenttab=True)
