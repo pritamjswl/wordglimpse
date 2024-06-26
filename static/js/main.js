4d23b474eb68155736997215be4de184c6c8b2b7
Pritam Jaiswal <156157726+pritamjswl@users.noreply.github.com>
2024-06-26 07:48:46 +0000

    nav

--- static/js/main.js
+++ static/js/main.js
@@ -7,4 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('#searchInput').focus();
         });
     });
+    // Theme Toggler
+    let themeToggler = document.querySelector('#themeToggler');
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        document.body.setAttribute('data-bs-theme', theme);
+        themeToggler.checked = (theme === 'dark');
+    }
+    themeToggler.addEventListener('change', () => {
+        let newTheme = themeToggler.checked ? 'dark' : 'light';
+        document.body.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
 });
