2c0b0f1debe8fe583a6d7e2e51bf66bfc700f04f
Pritam Jaiswal <156157726+pritamjswl@users.noreply.github.com>
2024-07-01 07:58:10 +0000

    Adding login req

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template, request
+import os
+from cs50 import SQL
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+from helpers import login_required
 
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure cs50 library to use SQLite database
+db = SQL("sqlite:///wg.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("user_id") is None:
+        return render_template("index.html")
+    return render_template("index.html", user_id=session["user_id"])
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -13,6 +37,11 @@ def login():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     return render_template("signup.html", auth=True)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 @app.route("/search")
 def search():
